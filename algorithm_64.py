d05d299b0fa55ae900bbf14963c017e144de4acf
paradoxoyh <494048906@qq.com>
2017-05-26 16:53:56 +0800

    Create algorithm_64.py

---
+++ algorithm_64.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def minPathSum(self, grid):
+        import math
+        m,n=len(grid),len(grid[0])
+        if m==n==0:
+            return 0
+        for i in range(1,m):
+            grid[i][0]+=grid[i-1][0]
+        for j in range(1,n):
+            grid[0][j]+=grid[0][j-1]
+        for i in range(1,m):
+            for j in range(1,n):
+                grid[i][j]=grid[i][j]+min(grid[i-1][j],grid[i][j-1])
+        return grid[m-1][n-1]
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        
