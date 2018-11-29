d1be31c61c5710314bb6a412eb51547c11e9ccbb
nickname <494048906@qq.com>
2018-11-29 16:27:29 +0800

    Update algorithm_134.cpp

--- algorithm_134.cpp
+++ algorithm_134.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int temp = 0;
-        for(int j = 0; j < gas.size(); j++)
+        for(int i = 0; i < gas.size(); i++)
+        {
+            gas[i] -= cost[i];            
+        }
+        for(int i = 0; i < gas.size(); i++)
         {
             int sum = 0;
-            for(int i = 0; i < gas.size(); i++)
+            for(int j = 0; j < gas.size(); j++)
             {
-                sum += gas[i];
-                sum -= cost[i];
+                sum += gas[j];
                 if(sum < 0)
-                {
-                    gas.push_back(gas[0]);
-                    gas.erase(gas.begin());
-                    cost.push_back(cost[0]);
-                    cost.erase(cost.begin());
-                    temp += 1;
                     break;
-                }
             }
+            gas.push_back(gas[0]);
+            gas.erase(gas.begin());
             if(sum >= 0)
-            {
-                return temp;
-                break;
-            }
+                return i;
         }
         return -1;
     }
