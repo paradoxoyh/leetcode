826a8b6d6d7d681cfe9b87151b58f42b76ee469f
nickname <494048906@qq.com>
2018-12-03 17:02:19 +0800

    Update algorithm_205.cpp

--- algorithm_205.cpp
+++ algorithm_205.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        map<char, char> cMaps, cMapt;
-        map<char, bool> bMaps, bMapt;
+        bool res;
+        return subIsomorphic(s, t) && subIsomorphic(t, s);
+    }
+private:
+    bool subIsomorphic(string s, string t){
+        unordered_map<char, char> cMap;
         for(int i = 0; i < s.size(); i++){
-            if(bMaps[s[i]] && cMaps[s[i]] != t[i])
+            if(cMap.count(s[i]) == 0)
+                cMap[s[i]] = t[i];
+            else if(cMap[s[i]] != t[i])
                 return false;
-            else
-            {
-                cMaps[s[i]] = t[i];
-                bMaps[s[i]] = true;
-            }
-            if(bMapt[t[i]] && cMapt[t[i]] != s[i])
-                return false;
-            else
-            {
-                cMapt[t[i]] = s[i];
-                bMapt[t[i]] = true;
-            }
         }
         return true;
     }
