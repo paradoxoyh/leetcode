7d92fa8c087ebdeefa6573c3c243c9adc5f99dec
nickname <494048906@qq.com>
2018-12-03 17:02:45 +0800

    Create algorithm_263.cpp

---
+++ algorithm_263.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isUgly(int num) {
+        num = subIsUgly(num, 2);
+        num = subIsUgly(num, 3);
+        num = subIsUgly(num, 5);
+        return (num == 1)? true: false;
+    }
+    int subIsUgly(int num, int k){
+        if(num <= 0)
+            return num;
+        while(true){
+            if(num%k == 0)
+                num = num / k;
+            else
+                break;
+        }
+        return num;
+    }
+};
