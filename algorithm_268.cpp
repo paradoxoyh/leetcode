1b83d0a40c5dd269b5446c98d1eac941abb317cf
nickname <494048906@qq.com>
2020-05-04 17:45:28 +0800

    Add files via upload

---
+++ algorithm_268.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int i = 0;
+        int res = 0;
+        nums.push_back(nums.size() + 2);
+        int invalidNum = nums.size() + 1;
+        for(i = 0; i < nums.size(); i++)
+        {
+            while(true)
+            {
+                int temp = nums[i];
+                if(temp == i || temp == invalidNum)
+                {
+                    if(temp == invalidNum)
+                        res = i;
+                    break;
+                }
+                nums[i] = nums[temp];
+                nums[temp] = temp;
+            }
+            int temp = nums[i];  
+        }
+        return res;
+    }
+};
